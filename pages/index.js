10459df5605462ffbfb2866f15fc12b396dff7d1
Jasir Parakkattil <30565023+jasir5449@users.noreply.github.com>
2022-02-14 10:07:56 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
+import {MongoClient} from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 import { useEffect,useState } from "react";
 
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
-];
 
 
 const HomePage =(props) =>{
+ 
    return (<MeetupList meetups={props.meetups} />)
 }
 
@@ -36,11 +22,19 @@ const HomePage =(props) =>{
 
 export async function getStaticProps (){
     
-    const response = await fetch('http://localhost:3000/api/new-meetup');
-    const result= await response.json();
+    // const response = await fetch('http://127.0.0.1:3000/api/new-meetup');
+    // const result= await response.json();
+    
+  const client = await MongoClient.connect('mongodb+srv://jasir5449:Jasir2299@cluster0.4b5wc.mongodb.net/meetups');
+  const db=  client.db();
+  const meetupCollection = db.collection('meetups');
+  const result = await meetupCollection.find().toArray();
+  client.close();
+
+  console.log(JSON.stringify(result));
     return {
         props:{
-            meetups:result.data
+            meetups:JSON.parse(JSON.stringify(result))
         },
         revalidate:1
     }
